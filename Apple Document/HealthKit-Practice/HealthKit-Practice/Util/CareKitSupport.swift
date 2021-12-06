425610c86daf06e75a8c8c17caf81bf3176d58b6
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-12-06 16:41:47 +0900

    [ADD] DataTypeCollectionViewController 구현

--- Apple Document/HealthKit-Practice/HealthKit-Practice/Util/CareKitSupport.swift
+++ Apple Document/HealthKit-Practice/HealthKit-Practice/Util/CareKitSupport.swift
@@ -10,6 +10,35 @@ import CareKitUI
 
 // MARK: - Chart Date UI
 
+func createChartWeeklyDateRangeLabel(lastDate: Date = Date()) -> String {
+    let calendar: Calendar = .current
+    let endOfWeekDate = lastDate
+    let startOfWeekDate = getLastWeekStartDate(from: endOfWeekDate)
+    
+    let monthDayDateFormatter = DateFormatter()
+    monthDayDateFormatter.dateFormat = "MMM d"
+    let monthDayYearDateFormatter = DateFormatter()
+    monthDayYearDateFormatter.dateFormat = "MMM d, yyyy"
+    
+    var startDateString = monthDayDateFormatter.string(from: startOfWeekDate)
+    var endDateString = monthDayYearDateFormatter.string(from: endOfWeekDate)
+    
+    // If the start and end dates are in the same month.
+    if calendar.isDate(startOfWeekDate, equalTo: endOfWeekDate, toGranularity: .month) {
+        let dayYearDateFormatter = DateFormatter()
+        
+        dayYearDateFormatter.dateFormat = "d, yyyy"
+        endDateString = dayYearDateFormatter.string(from: endOfWeekDate)
+    }
+    
+    // If the start and end dates are in different years.
+    if !calendar.isDate(startOfWeekDate, equalTo: endOfWeekDate, toGranularity: .year) {
+        startDateString = monthDayYearDateFormatter.string(from: startOfWeekDate)
+    }
+    
+    return String(format: "%@-%@", startDateString, endDateString)
+}
+
 private func createMonthDayDateFormatter() -> DateFormatter {
     let dateFormatter = DateFormatter()
     
@@ -24,6 +53,35 @@ func createChartDateLastUpdatedLabel(_ dateLastUpdated: Date) -> String {
     dateFormatter.dateStyle = .medium
     
     return "last updated on \(dateFormatter.string(from: dateLastUpdated))"
+}
+
+func createHorizontalAxisMarkers(lastDate: Date = Date(), useWeekdays: Bool = true) -> [String] {
+    let calendar: Calendar = .current
+    let weekdayTitles = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+    var titles: [String] = []
+    
+    if useWeekdays {
+        titles = weekdayTitles
+        
+        let weekday = calendar.component(.weekday, from: lastDate)
+        
+        return Array(titles[weekday..<titles.count]) + Array(titles[0..<weekday])
+    } else {
+        let numberOfTitles = weekdayTitles.count
+        let endDate = lastDate
+        let startDate = calendar.date(byAdding: DateComponents(day: -(numberOfTitles - 1)), to: endDate)!
+        
+        let dateFormatter = createMonthDayDateFormatter()
+        
+        var date = startDate
+        
+        while date <= endDate {
+            titles.append(dateFormatter.string(from: date))
+            date = calendar.date(byAdding: .day, value: 1, to: date)!
+        }
+        
+        return titles
+    }
 }
 
 func createHorizontalAxisMarkers(for dates: [Date]) -> [String] {
---
+++ Apple Document/HealthKit-Practice/HealthKit-Practice/ViewController/MobilityChartDataViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MobilityChartDataViewController.swift
+//  HealthKit-Practice
+//
+//  Created by SHIN YOON AH on 2021/12/06.
+//
+
+import UIKit
+import HealthKit
+
+class MobilityChartDataViewController {
+    
+}
