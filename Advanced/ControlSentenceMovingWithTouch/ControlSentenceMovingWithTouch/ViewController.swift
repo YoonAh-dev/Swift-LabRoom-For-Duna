d01a90570c474483c0472dd49f26bbef949a76f0
YoonAh-dev <tlsdbsdk05250@gmail.com>
2022-10-20 14:23:15 +0900

    [ADD] 문장 사이 움직임 관련 프로젝트 추가

---
+++ Advanced/ControlSentenceMovingWithTouch/ControlSentenceMovingWithTouch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ControlSentenceMovingWithTouch
+//
+//  Created by SHIN YOON AH on 2022/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
